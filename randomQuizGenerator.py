6d077387f48e1704985d856414383ea4d0a85d36
Lam Le <LamMinhLe@Lams-MacBook-Pro.local>
2016-03-16 23:29:42 -0400

    attempt 6

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -51,9 +51,10 @@ for questionNum in range(50):
     random.shuffle(answerOptions)
 
     quizFile.write('%s. what is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
-for i in range(4):
+    for i in range(4):
     quizFile.write('%s. %s\n' % ('ABCD'[i], answerOptions[i]))
     quizFile.write('\n')
+
     answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswers)]))
     quizFile.close()
     answerKeyFile.close()
